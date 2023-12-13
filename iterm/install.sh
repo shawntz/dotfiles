673fd3515bbb9cb05c5ef85f8385dea56c0b305a
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2023-12-12 20:29:41 -0800

    :wrench: change omz config destination to env.sh

--- iterm/install.sh
+++ iterm/install.sh
@@ -15,15 +15,15 @@ rm omz_installer.sh
 rm ~/.zshrc
 mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
 
-echo 'export ZSH="$HOME/.oh-my-zsh"' >>~/.zshrc
+echo 'export ZSH="$HOME/.oh-my-zsh"' >>~/.env.sh
 
 # Install Powerlevel10k for Oh My Zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >>~/.zshrc
+echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >>~/.env.sh
 
 # Load in p10k config file
 # p10k configure
-echo 'POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' >>! ~/.zshrc
+echo 'POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' >>! ~/.env.sh
 
 # Install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions \
@@ -34,10 +34,10 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
 ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Activate plugins
-echo "plugins=(git zsh-syntax-highlighting zsh-autosuggestions)" >>~/.zshrc
+echo "plugins=(git zsh-syntax-highlighting zsh-autosuggestions)" >>~/.env.sh
 
 # Activate oh-my-zsh
-echo "source $ZSH/oh-my-zsh.sh" >>~/.zshrc
+echo "source $ZSH/oh-my-zsh.sh" >>~/.env.sh
 
 # Restart
 source ~/.zshrc
