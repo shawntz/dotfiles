df1afdf78cfb0169946247bc96de2b231352f744
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2023-12-12 20:21:19 -0800

    :wrench: update vscode setup recipe

--- vscode/config.sh
+++ vscode/config.sh
@@ -3,5 +3,5 @@
 fileicon set "/Applications/Visual Studio Code.app" $HOME/.init/citylights.icns
 
 # https://citylights.xyz
-git clone https://github.com/Yummygum/city-lights-syntax-vsc.git ~/.vscode/extensions/city-lights-theme
 git clone https://github.com/Yummygum/city-lights-icons-vsc.git ~/.vscode/extensions/
+git clone https://github.com/Yummygum/city-lights-syntax-vsc.git ~/.vscode/extensions/city-lights-theme
