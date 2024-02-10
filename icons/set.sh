5a2eb20115d2ede872daca011be9ff6f0b19140c
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2024-02-10 12:09:00 -0800

    :wrench: update spotify icon

--- icons/set.sh
+++ icons/set.sh
@@ -6,6 +6,6 @@ fileicon set "/Applications/iTerm.app" $HOME/.init/iTerm2-nord-chevron.icns
 fileicon set "/Applications/Visual Studio Code.app" $HOME/.init/citylights.icns
 fileicon set "/Applications/Arc.app" $HOME/.init/Arc-Developer-Glow.png
 fileicon set "/Applications/RStudio.app" $HOME/.init/R-DarkIcon-Rect.icns
-fileicon set "/Applications/Spotify.app" $HOME/.init/Spotify_Dark_Alt.icns
+fileicon set "/Applications/Spotify.app" $HOME/.init/spotify_alt_macos_bigsur_icon_189704.icns
 
 echo "done!"
