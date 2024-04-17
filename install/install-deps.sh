3efda26da70597cd1b70adbaa9d4fc0f475158a3
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2024-04-17 11:21:38 -0700

    :wrench: update deps

--- install/install-deps.sh
+++ install/install-deps.sh
@@ -18,6 +18,7 @@ brew install jsonpp
 brew install neovim
 brew install node
 brew install pandoc
+brew install php
 brew install pinentry-mac
 brew install rclone
 brew install tmux
@@ -32,39 +33,39 @@ brew install z
 brew install --cask adobe-creative-cloud
 brew install --cask aldente
 brew install --cask alfred
-brew install --cask arc
 brew install --cask bartender
+brew install --cask bibdesk
 brew install --cask caffeine
 brew install --cask dropbox
-brew install --cask firefox
 brew install --cask google-chrome
 brew install --cask hazeover
+brew install --cask istat-menus
 brew install --cask iterm2
 brew install --cask keycastr
 brew install --cask kindle
 brew install --cask mactex
+brew install --cask microsoft-auto-update
 brew install --cask microsoft-office
 brew install --cask microsoft-remote-desktop
 brew install --cask middleclick
 brew install --cask min
 brew install --cask notion
-brew install --cask notion-calendar
-brew install --cask obsidian
 brew install --cask postman
 brew install --cask psychopy
 brew install --cask r
 brew install --cask rectangle
 brew install --cask rstudio
 brew install --cask slack
-brew install --cask spotify
 brew install --cask visual-studio-code
 brew install --cask xquartz
 brew install --cask zoom
-brew install --cask zotero
 
 brew tap homebrew/cask-fonts
 brew install --cask font-tex-gyre-pagella
 brew install --cask font-tex-gyre-termes
+
+# start background services
+brew services start php
 
 # Remove outdated versions from the cellar.
 brew cleanup
