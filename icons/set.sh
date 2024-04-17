c2573cdaefb247b631b4ce3d24ed66c6b2106476
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2024-04-17 11:20:12 -0700

    :wrench: purge old app icons

--- icons/set.sh
+++ icons/set.sh
@@ -4,8 +4,6 @@ echo "setting custom app icons..."
 
 fileicon set "/Applications/iTerm.app" $HOME/.init/iTerm2-nord-chevron.icns
 fileicon set "/Applications/Visual Studio Code.app" $HOME/.init/citylights.icns
-fileicon set "/Applications/Arc.app" $HOME/.init/Arc-Developer-Glow.png
 fileicon set "/Applications/RStudio.app" $HOME/.init/R-DarkIcon-Rect.icns
-fileicon set "/Applications/Spotify.app" $HOME/.init/spotify_alt_macos_bigsur_icon_189704.icns
 
 echo "done!"
