caeb2f8b5e1d9226950d9c5cd83b6fd7a43b85ea
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2024-08-04 00:43:50 -0400

    :wrench: refactor zsh configs

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -27,7 +27,7 @@ case ${HIST_STAMPS-} in
 esac
 
 ## History file configuration
-[ -z "$HISTFILE" ] && HISTFILE="$HOME/.zsh_history"
+[ -z "$HISTFILE" ] && HISTFILE="$HOME/.zhistory"
 HISTSIZE=50000
 SAVEHIST=100000
 
@@ -38,4 +38,8 @@ setopt hist_ignore_dups       # ignore duplicated commands history list
 setopt hist_ignore_space      # ignore commands that start with space
 setopt hist_verify            # show command with history expansion to user before running it
 setopt inc_append_history     # add commands to HISTFILE in order of execution
-# setopt share_history          # share command history data
+setopt share_history          # share command history data
+
+bindkey "^[[A" history-search-backward
+bindkey "^[[B" history-search-forward
+
