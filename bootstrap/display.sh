2f9e12936d1eab5e71bb84142b024dc488519650
shawn schwartz <s@shawnts.com>
2024-08-14 20:28:40 -0700

    :wrench: fix display scaling

--- bootstrap/display.sh
+++ bootstrap/display.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # enable HiDPI
-gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "[{'Gdk/WindowScalingFactor', <2>}]"
-gsettings set org.gnome.desktop.interface scaling-factor 2
+gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "[{'Gdk/WindowScalingFactor', <1>}]"
+gsettings set org.gnome.desktop.interface scaling-factor 1
