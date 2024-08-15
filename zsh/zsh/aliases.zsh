d3449957819e7eb3f8618a70406dbee6804659e3
shawn schwartz <s@shawnts.com>
2024-08-14 18:34:23 -0700

    :wrench: update zsh aliases

--- zsh/zsh/aliases.zsh
+++ zsh/zsh/aliases.zsh
@@ -10,12 +10,17 @@ alias bbb='cd ../../..'
 alias bbbb='cd ../../../..'
 alias hg='history | grep'
 alias ls='eza --all --color=always --sort=name --long --no-user --icons=always --no-permissions'
-alias c='clear'
+alias cl='clear'
 alias s='source ~/.zshrc'
 alias jj='pbpaste | jsonpp | pbcopy'
 alias trim="awk '{\$1=\$1;print}'"
 alias g='lazygit'
 alias f='yazi'
+alias en='/usr/bin/google-chrome-stable --app-id=bngdbpbgpaflcededbiphmicbhmcdbmf & disown'
+alias e='/usr/bin/google-chrome-stable --app-id=fmgjjmmmlfnkbppncabfkddbjimcfncm & disown'
+alias c='/usr/bin/google-chrome-stable --app-id=kjbdgfilnfhdoflbpgamdcdgpehopbep & disown'
+alias gh='/usr/bin/google-chrome-stable --app-id=mjoklplbddabcmpepnokjaffbmgbkkgg & disown'
+alias d='/usr/bin/google-chrome-stable --app-id=aghbiahbpaijignceidepookljebhfak & disown'
 
 # FUNCTIONS -------------------------------------------------------------------
 function cs () {
