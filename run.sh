f8686ed76d98d2256dfe906ebef0e655205335b6
shawn schwartz <shawn@mercury.attlocal.net>
2024-08-18 15:15:35 -0700

    :rocket: refactor run script

--- run.sh
+++ run.sh
@@ -2,5 +2,13 @@ if type git > /dev/null; then
     git clone https://github.com/shawntschwartz/dotfiles ~/dotfiles
     chmod +x ~/dotfiles
     cd ~/dotfiles
-    ./install.sh
+    ./install
+else
+    curl -LO https://github.com/shawntschwartz/dotfiles/archive/macos.zip
+    unzip dotfiles-macos.zip
+    rm -rf dotfiles-macos.zip
+    mv dotfiles-macos ~/dotfiles
+    chmod +x ~/dotfiles
+    cd ~/dotfiles
+    ./install
 fi
