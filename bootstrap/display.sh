4548346b4b6188135c60c2ac51e4890697c24ef6
shawn schwartz <s@shawnts.com>
2024-09-03 22:54:33 -0700

    remove `bootstrap/display.sh`

--- bootstrap/display.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# enable HiDPI
-gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "[{'Gdk/WindowScalingFactor', <1>}]"
-gsettings set org.gnome.desktop.interface scaling-factor 1
