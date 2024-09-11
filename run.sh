532249794d307f13fa33c6a27da683ee16114c3b
shawn schwartz <s@shawnts.com>
2024-09-11 16:31:28 -0700

    :wrench: update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 if type git > /dev/null; then
+    sudo xcodebuild -license
     git clone https://github.com/shawntschwartz/dotfiles ~/dotfiles
     chmod +x ~/dotfiles
     cd ~/dotfiles
     ./install
 else
+    sudo xcodebuild -license
     curl -LO https://github.com/shawntschwartz/dotfiles/archive/macos.zip
     unzip dotfiles-macos.zip
     rm -rf dotfiles-macos.zip
