d5cbc9380c235b2d91b61683d887796d9aafa291
shawn <s@shawnts.com>
2024-09-15 00:57:35 -0700

    :hammer: add hammerspoon config

---
+++ hammerspoon/.hammerspoon/keyboard/hyper-apps-defaults.lua
@@ -0,0 +1,17 @@
+-- Default keybindings for launching apps in Hyper Mode
+--
+-- To launch _your_ most commonly-used apps via Hyper Mode, create a copy of
+-- this file, save it as `hyper-apps.lua`, and edit the table below to configure
+-- your preferred shortcuts.
+return {
+  { 'a', 'Music' },             -- "A" for "Apple Music"
+  { 'b', 'Safari' },     -- "B" for "Browser"
+  { 'c', 'GCal' }, -- "D" for "Do!" ... or "Done!"
+  { 'e', 'Visual Studio Code' },              -- "E" for "Editor"
+  { 'f', 'Finder' },            -- "F" for "Finder"
+  { 'g', 'Gmail' },       -- "G" for "Gmail"
+  { 's', 'Slack' },             -- "S" for "Slack"
+  { 'k', 'kitty' },             -- "T" for "Terminal"
+  { 'r', 'RStudio' },             -- "T" for "Terminal"
+  { 'p', 'Positron' },             -- "T" for "Terminal"
+}
