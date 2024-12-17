ea12b1bcc2cdfac0d14fe09ca80f6783723d6206
Shawn Schwartz <mail@shawntz.com>
2024-12-17 00:54:51 -0800

    add(nix module): users WIP

---
+++ nix-darwin/modules/users.nix
@@ -0,0 +1,35 @@
+{ pkgs, hostname, username, ... }:
+
+#################################
+###    Host & Users Config    ###
+#################################
+
+{
+  networking.hostName = hostname;
+  networking.computerName = hostname;
+  system.defaults.smb.NetBIOSName = hostname;
+
+  users.users."${username}" = {
+    home = "/Users/${username}";
+    description = username;
+    #shell = pkgs.fish;
+  };
+
+  nix.settings.trusted-users = [ username ];
+
+  home-manager.useGlobalPkgs = true; # Use nixpkgs from the flake
+  home-manager.useUserPackages = true; # Allow per-user packages
+
+  home-manager.users.${username} = { pkgs, lib, ... }: {
+    home.stateVersion = "24.11"; # Match this to your system
+    programs.fish.enable = true; # Enable fish shell
+    home.packages = [ pkgs.fish ]; # Add fish shell explicitly
+
+    programs.neovim = {
+      enable = true;
+      viAlias = true;
+	    vimAlias = true;
+	    vimdiffAlias = true;
+	  };
+  };
+}
