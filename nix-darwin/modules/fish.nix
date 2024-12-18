75ed25d0e795bb3a1ecca705e314c55eba84e181
Shawn Schwartz <mail@shawntz.com>
2024-12-17 22:08:55 -0800

    chore(flake): fish.nix

--- nix-darwin/modules/fish.nix
+++ nix-darwin/modules/fish.nix
@@ -10,6 +10,7 @@
         # silence shell messages
         printf '\33c\e[3J'  # hide 'last login' message
         set -g fish_greeting ""
+	      echo $0
         # neofetch
         neofetch
         # zoxide
@@ -17,43 +18,42 @@
       '';
       functions = {
         # neovim
-        v = "nvim -w ~/.vimlog $argv";
+        v = "kitty @set-colors background=#282828 && nvim -w ~/.vimlog $argv && kitty @set-colors background=#1a1b26";
+        vim = "kitty @set-colors background=#282828 && nvim -w ~/.vimlog $argv && kitty @set-colors background=#1a1b26";
         vi = "fd --type f --hidden --exclude .git | fzf-tmux -m --preview='bat --color=always {}' -p | xargs nvim";
-        vim = "nvim -w ~/.vimlog $argv";
 
         # navigation
         c = "zi $argv[1]";
-        cl = "clear";
         cd = "z $argv[1]";
         b = "cd ..";
         bb = "cd ../..";
         bbb = "cd ../../..";
         bbbb = "cd ../../../..";
-        l = "ls $argv[1]";
-        ls = "eza $argv[1] --all --color=always --sort=name --long --no-user --icons=always --no-permissions";
+        l = "eza $argv[1] --all --color=always --sort=name --long --no-user --icons=always --no-permissions";
         home = "cd ~";
+        d = "clear";
         dots = "cd ~/Developer/dotfiles";
         dotfiles = "cd ~/Developer/dotfiles";
         dev = "cd ~/Developer";
         desk = "cd ~/Desktop";
         docs = "cd ~/Documents";
         dl = "cd ~/Downloads";
-        md = "mkdir -p";
-        t = "touch";
+        md = "mkdir -p $argv[1]";
+        t = "touch $argv[1]";
         x = "exit";
         k = "killall kitty";
         o = "open .";
         restart = "sudo reboot";
         bye = "sudo shutdown -r now";
-        get = "curl -O -L";
+        get = "curl -O -L $argv[1]";
         ssh = "kitty +kitten ssh";
 
         # tools
         vs = "code -g .";
         s = "source ~/.config/fish/config.fish";
-        trim = "awk '{\$1=\$1;print}'";
 	      g = "lazygit";
-	      f = "yazi";
+	      f = "yazi | xargs nvim";
+	      fz = "fzf | xargs nvim";
 	      stfu = "osascript -e 'set volume output muted true'";
 	      afk = "/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend";
         chromekill = "ps ux | grep '[C]hrome Helper --type=renderer' | grep -v extension-process | tr -s ' ' | cut -d ' ' -f2 | xargs kill";
@@ -61,7 +61,7 @@
 	      flake = "darwin-rebuild switch --flake ~/Nix --show-trace";
 
 	      # functions
-	      cs = ''
+	      cl = ''
           z $argv[1]
           eza --all --color=always --sort=name --no-filesize --no-user --icons=always --no-permissions
 	      '';
