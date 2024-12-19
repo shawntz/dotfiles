e29c87786ed4afc95539cd498d1f2633393c8062
Shawn Schwartz <mail@shawntz.com>
2024-12-18 16:42:34 -0800

    enhance(kitty): style

--- nix-darwin/modules/kitty.nix
+++ nix-darwin/modules/kitty.nix
@@ -8,7 +8,7 @@
       font.name = "JetBrainsMono Nerd Font";
       settings = {
         allow_remote_control = "yes";
-        font_size = 24.0;
+        font_size = 22.0;
         hide_window_decorations = "titlebar-only";
         macos_window_resizable = "yes";
         macos_show_window_title_in = "menubar";
@@ -17,10 +17,10 @@
         macos_menubar_title_max_length = 0;
         macos_quit_when_last_window_closed = "yes";
         macos_colorspace = "displayp3";
-        window_padding_height = 30;
-        window_padding_width = 5;
-        background_opacity = 0.8;
-        background_blur = 64;
+        # window_padding_height = 30;
+        # window_padding_width = 5;
+        background_opacity = 0.4;
+        background_blur = 128;
         confirm_os_window_close = -0;
         copy_on_select = true;
         clipboard_control = "write-clipboard read-clipboard write-primary read-primary";
