1c57b2bba1c92cc02affa6b3735cabf07dce8d59
Shawn Schwartz <mail@shawntz.com>
2024-12-18 16:44:36 -0800

    bf(flake): update neovim alias to work with tmux in fish.nix

--- nix-darwin/modules/fish.nix
+++ nix-darwin/modules/fish.nix
@@ -18,7 +18,7 @@
       '';
       functions = {
         # neovim
-        v = "kitty @set-colors background=#282828 && nvim -w ~/.vimlog $argv && kitty @set-colors background=#1a1b26";
+        v = "nvim -w ~/.vimlog $argv";
         vim = "kitty @set-colors background=#282828 && nvim -w ~/.vimlog $argv && kitty @set-colors background=#1a1b26";
         vi = "fd --type f --hidden --exclude .git | fzf-tmux -m --preview='bat --color=always {}' -p | xargs nvim";
 
