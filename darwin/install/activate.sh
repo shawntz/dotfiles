4533d661ca8b8ed6ffe56033a38b9ea3d3ffe189
Shawn Schwartz <shawn@shawnschwartz.com>
2025-09-08 01:30:53 -0700

    disable some apps in the activate.sh script for testing

--- darwin/install/activate.sh
+++ darwin/install/activate.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
-open -a 1Password.app
+# open -a 1Password.app
 open -a Aerospace.app
 open -a Google\ Drive.app
 open -a Hammerspoon.app
 open -a Karabiner-Elements.app
-open -a Raycast.app
-open -a Alfred\ 5.app
+# open -a Raycast.app
+# open -a Alfred\ 5.app
 open -a Shortcat.app
